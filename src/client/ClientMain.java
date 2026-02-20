08d66c23aa1aef26ed8994c522de225f828c301e
gCap-pixel <gcappellin9@gmail.com>
2026-02-20 13:07:35 +0100

    modifica del ciclo while per poter mandare più messaggi

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,6 +1,9 @@
 package client;
 
+import java.util.Scanner;
+
 public class ClientMain {
+<<<<<<< HEAD
     private static String stringa = "true";
 
     public static void main(String[] args) {
@@ -17,5 +20,30 @@ public class ClientMain {
             client.chiudi();
         }
 
+=======
+    private static String stringa = "";
+
+    public static void main(String[] args) {
+        Client client = new Client("gab");
+        Scanner tastiera = new Scanner(System.in);
+
+        client.connetti();
+
+        while(stringa != null && !stringa.equals("stop")) {
+            System.out.print("Scrivi messaggio per il server: ");
+            stringa = tastiera.nextLine();
+
+            client.invia(stringa);
+
+            if (!stringa.equals("stop")) {
+                client.ricevi();
+                stringa = client.getTesto();
+            }
+        }
+
+        tastiera.close();
+        client.chiudi();
+        System.out.println("Comunicazione chiusa.");
+>>>>>>> 8116581 (modifica del ciclo while per poter mandare più messaggi)
     }
 }
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,6 +1,7 @@
 package server;
 
 public class ServerMain {
+<<<<<<< HEAD
 
     private static String stringa = "";
 
@@ -8,6 +9,12 @@ public class ServerMain {
         int port = 1255;
         Server server = new Server(port);
 
+=======
+    private static String stringa = "";
+
+    public static void main(String[] args) {
+        Server server = new Server(1255);
+>>>>>>> 8116581 (modifica del ciclo while per poter mandare più messaggi)
         server.attendi();
 
         while (stringa != null && !stringa.equals("stop")) {
@@ -19,5 +26,6 @@ public class ServerMain {
             }
         }
         server.chiudi();
+        System.out.println("Server spento.");
     }
 }
