73d8c74f4dfdd62a38ca4d9f64b86c69bcd48488
Gabriele <gcappellin9@gmail.com>
2026-02-20 12:19:44 +0100

    risuluzioni dei conflitti su ServerMain.java

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,20 +1,11 @@
 package server;
 
 public class ServerMain {
-<<<<<<< HEAD
 
-    private static String stringa = "";
-
-    public static void main(String[] args) {
-        int port = 1255;
-        Server server = new Server(port);
-
-=======
     private static String stringa = "";
 
     public static void main(String[] args) {
         Server server = new Server(1255);
->>>>>>> 8116581 (modifica del ciclo while per poter mandare più messaggi)
         server.attendi();
 
         while (stringa != null && !stringa.equals("stop")) {
