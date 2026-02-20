60809f16c8089fd4d051ead0b9d222cc1d3f8f39
Gabriele <gcappellin9@gmail.com>
2026-02-20 12:18:09 +0100

    risoluzione conflitti su ClientMain.java

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -3,24 +3,7 @@ package client;
 import java.util.Scanner;
 
 public class ClientMain {
-<<<<<<< HEAD
-    private static String stringa = "true";
-
-    public static void main(String[] args) {
-        Client client = new Client("gab");
-        client.connetti();
-        while (stringa != null && !stringa.equals("stop")) {
-            client.invia("ciao");
-            client.ricevi();
-            stringa = client.getTesto();
-            if (stringa == null) {
-                System.out.println("Connessione persa col server.");
-                break;
-            }
-            client.chiudi();
-        }
-
-=======
+    
     private static String stringa = "";
 
     public static void main(String[] args) {
@@ -44,6 +27,5 @@ public class ClientMain {
         tastiera.close();
         client.chiudi();
         System.out.println("Comunicazione chiusa.");
->>>>>>> 8116581 (modifica del ciclo while per poter mandare più messaggi)
     }
 }
